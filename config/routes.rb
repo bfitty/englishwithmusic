3697221433c6c2ca5918632502735d74d53e91ff
Ben Fitterman <ben.fitterman@gmail.com>
2014-06-09 17:38:32 -0400

    created pages and added links for a nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Englishwithmusic::Application.routes.draw do
   root "pages#home"
+  get  "about" => "pages#about"
+  get "songs"  => "pages#songs"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
